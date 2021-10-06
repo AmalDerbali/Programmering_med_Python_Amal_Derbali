65bd2a68e2ad7c2f35606c91773b3259f9affb1f
AmalDerbali <89392991+AmalDerbali@users.noreply.github.com>
2021-10-06 09:41:05 +0200

    minor changes

--- Labs/Lab3/sphere.py
+++ Labs/Lab3/sphere.py
@@ -1,7 +1,5 @@
 from geometry_shapes import Circle
 import math
-import matplotlib.pyplot as plt
-import numpy as np
 
 
 
@@ -62,14 +60,5 @@ class Sphere (Circle):
         return f"The radius of the circle is: {self.radius}. The geometric center is: ({self.x_value}, {self.y_value}, {self.z_value})."
     
 
-fig = plt.figure(dpi=100)
-ax = fig.gca(projection='3d')
-
-u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-x = np.cos(u)*np.sin(v)
-y = np.sin(u)*np.sin(v)
-z = np.cos(v)
-ax.plot_wireframe(x, y, z, color="green")
-ax.set_title("Sphere")
-plt.show()     
+    
     
